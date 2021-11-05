192b731ee71a11c4fac3a9695622b94d55fd3fde
vitaly <vitaly.zubentsov@ya.ru>
2021-11-05 13:53:45 +0300

    use spring container, setting by xml file

--- spring-demo-one/src/com/zubentsov/springdemo/MyApp.java
+++ spring-demo-one/src/com/zubentsov/springdemo/MyApp.java
@@ -4,7 +4,7 @@ public class MyApp {
 
 	public static void main(String[] args) {
 		
-		Coach theCouch = new BaseballCoach();
+		Coach theCouch = new TrackCoach();
 		System.out.println(theCouch.getDialyWorcout());
 
 	}
---
+++ spring-demo-one/src/com/zubentsov/springdemo/TrackCoach.java
@@ -0,0 +1,10 @@
+package com.zubentsov.springdemo;
+
+public class TrackCoach implements Coach {
+
+	@Override
+	public String getDialyWorcout() {
+		return "Hard work on track";
+	}
+
+}
