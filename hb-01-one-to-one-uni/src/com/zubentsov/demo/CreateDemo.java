f83a2f185810a3d8b5db2ffb6120891afb05d540
vitaly <vitaly.zubentsov@ya.ru>
2021-11-23 21:51:23 +0300

    Hibernate advanced mappings. OneToOne uni create

---
+++ hb-01-one-to-one-uni/src/com/zubentsov/demo/CreateDemo.java
@@ -0,0 +1,59 @@
+package com.zubentsov.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.zubentsov.demo.entity.Instructor;
+import com.zubentsov.demo.entity.InstructorDetail;
+
+public class CreateDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+
+			// create object
+			InstructorDetail instructorDetail = new InstructorDetail("youtubeAny", "play football");
+
+			Instructor instructor = new Instructor("Ivan", "Ivanov", "ivani@mail.ru");
+
+			// associate the objects
+			instructor.setInstructorDetail(instructorDetail);
+
+			session.beginTransaction();
+
+			// this will ALSO save the instructorDetail because
+			// CascadType.ALL
+			session.save(instructor);
+
+			session.getTransaction().commit();
+
+			// CHECK
+			session = factory.getCurrentSession();
+
+			session.beginTransaction();
+
+			InstructorDetail instructorDetailFromBD = session.get(InstructorDetail.class, instructorDetail.getId());
+
+			session.getTransaction().commit();
+
+			System.out.println("Instructor detail from bd :" + instructorDetailFromBD);
+
+		} catch (Exception ex) {
+			System.out.println(ex);
+		} finally {
+			session.close();
+			factory.close();
+		}
+
+	}
+
+}
