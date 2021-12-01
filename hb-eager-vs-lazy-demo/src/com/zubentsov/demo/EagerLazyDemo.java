1cfe185b4c94c698b9ab0b210a04e3e33ab534c1
vitaly <vitaly.zubentsov@ya.ru>
2021-12-01 21:11:25 +0300

    Hibernate advanced mappings. Eager fetch

---
+++ hb-eager-vs-lazy-demo/src/com/zubentsov/demo/EagerLazyDemo.java
@@ -0,0 +1,48 @@
+package com.zubentsov.demo;
+
+import org.hibernate.Session;
+import org.hibernate.SessionFactory;
+import org.hibernate.cfg.Configuration;
+
+import com.zubentsov.demo.entity.Course;
+import com.zubentsov.demo.entity.Instructor;
+import com.zubentsov.demo.entity.InstructorDetail;
+
+public class EagerLazyDemo {
+
+	public static void main(String[] args) {
+
+		// create session factory
+		SessionFactory factory = new Configuration().configure("hibernate.cfg.xml").addAnnotatedClass(Instructor.class)
+				.addAnnotatedClass(InstructorDetail.class).addAnnotatedClass(Course.class).buildSessionFactory();
+
+		// create session
+		Session session = factory.getCurrentSession();
+
+		try {
+			
+			session.beginTransaction();
+
+			// get instructor by id 
+			int instructorId = 1;
+			
+			Instructor instructor = session.get(Instructor.class, instructorId);
+					
+			session.getTransaction().commit();
+
+			//try use instructor courses after commit
+			
+			System.out.println("Instructor :" + instructor);
+			
+			System.out.println("Instructor courses " + instructor.getCourses());
+
+		} catch (Exception ex) {
+			System.out.println(ex);
+		} finally {
+			session.close();
+			factory.close();
+		}
+
+	}
+
+}
