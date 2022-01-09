ba61eae9042becb334fee2c5e31ee18b36daf0f7
vitaly <vitaly.zubentsov@ya.ru>
2022-01-09 14:58:40 +0300

    Spring Security. Add jdbc database authentication

--- spring-security-demo/src/main/java/com/zubentsov/springsecurity/demo/config/DemoConfigApp.java
+++ spring-security-demo/src/main/java/com/zubentsov/springsecurity/demo/config/DemoConfigApp.java
@@ -1,28 +1,85 @@
 package com.zubentsov.springsecurity.demo.config;
 
+import java.util.logging.Logger;
+
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
+import org.springframework.core.env.Environment;
 import org.springframework.web.servlet.ViewResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 import org.springframework.web.servlet.view.InternalResourceViewResolver;
 
+import com.mchange.v2.c3p0.ComboPooledDataSource;
+
 @Configuration
 @EnableWebMvc
-@ComponentScan(basePackages="com.zubentsov.springsecurity.demo")
+@ComponentScan(basePackages = "com.zubentsov.springsecurity.demo")
+@PropertySource("classpath:persistence-mysql.properties")
 public class DemoConfigApp {
-	
+
+	// set up variable to hold the properties
+
+	@Autowired
+	private Environment env;
+
+	// set up logger for diagnostics
+
+	private Logger logger = Logger.getLogger(this.getClass().getName());
+
 	// define a bean for ViewResolver
 
-		@Bean
-		public ViewResolver viewResolver() {
-			
-			InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
-			
-			viewResolver.setPrefix("/WEB-INF/view/");
-			viewResolver.setSuffix(".jsp");
-			
-			return viewResolver;
+	@Bean
+	public ViewResolver viewResolver() {
+
+		InternalResourceViewResolver viewResolver = new InternalResourceViewResolver();
+
+		viewResolver.setPrefix("/WEB-INF/view/");
+		viewResolver.setSuffix(".jsp");
+
+		return viewResolver;
+	}
+
+	// define a bean for our security datasource
+
+	@Bean
+	public DataSource securityDataSource() {
+
+		// create connection pool
+		ComboPooledDataSource securityDataSource = new ComboPooledDataSource();
+
+		// set jdbs driver class
+		try {
+			securityDataSource.setDriverClass(env.getProperty("jdbc.driver"));
+		} catch (Exception ex) {
+			throw new RuntimeException(ex);
 		}
+
+		// log the connection properties
+		logger.info(">>> jdbc.url=" + env.getProperty("jdbc.url"));
+		logger.info(">>> jdbc.user=" + env.getProperty("jdbc.user"));
+
+		// set database connection properties
+		securityDataSource.setJdbcUrl(env.getProperty("jdbc.url"));
+		securityDataSource.setUser(env.getProperty("jdbc.user"));
+		securityDataSource.setPassword(env.getProperty("jdbc.password"));
+
+		// set connection pool properties
+
+		securityDataSource.setInitialPoolSize(getIntProperty("connection.pool.initialPoolSize"));
+		securityDataSource.setMinPoolSize(getIntProperty("connection.pool.minPoolSize"));
+		securityDataSource.setMaxPoolSize(getIntProperty("connection.pool.maxPoolSize"));
+		securityDataSource.setMaxIdleTime(getIntProperty("connection.pool.maxIdleTime"));
+		return securityDataSource;
+
+	}
+
+	public int getIntProperty(String nameOfProperty) {
+		return Integer.parseInt(env.getProperty(nameOfProperty));
+	}
 
 }
--- spring-security-demo/src/main/java/com/zubentsov/springsecurity/demo/config/DemoSecurityConfig.java
+++ spring-security-demo/src/main/java/com/zubentsov/springsecurity/demo/config/DemoSecurityConfig.java
@@ -1,49 +1,38 @@
 package com.zubentsov.springsecurity.demo.config;
 
+import javax.sql.DataSource;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
 import org.springframework.security.config.annotation.web.builders.HttpSecurity;
 import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
 import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
-import org.springframework.security.core.userdetails.User;
-import org.springframework.security.core.userdetails.User.UserBuilder;
-
 
 @Configuration
 @EnableWebSecurity
 public class DemoSecurityConfig extends WebSecurityConfigurerAdapter {
 
+	//add a reference to our security data source 
+	@Autowired
+	private DataSource securityDataSource;
+	
 	@Override
 	protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+
+	//use jdbc authentication
 		
-		//add our users for in memory authentication
-		
-		UserBuilder users = User.withDefaultPasswordEncoder();
-		auth.inMemoryAuthentication()
-		.withUser(users.username("Ivan").password("test123").roles("EMPLOYEE"))
-		.withUser(users.username("Igor").password("test123").roles("EMPLOYEE","MANAGER"))
-		.withUser(users.username("Gleb").password("test123").roles("EMPLOYEE","ADMIN"));
-		
+		auth.jdbcAuthentication().dataSource(securityDataSource); 
 	}
 
 	@Override
 	protected void configure(HttpSecurity http) throws Exception {
-		 
-		http.authorizeRequests()
-		.antMatchers("/").hasRole("EMPLOYEE")
-		.antMatchers("/leaders/**").hasRole("MANAGER")
-		.antMatchers("/systems/**").hasRole("ADMIN")
-		.and()
-		.formLogin().loginPage("/loginPage").loginProcessingUrl("/authenticateTheUser").permitAll()
-		.and()
-		.logout().permitAll()
-		.and()
-		.exceptionHandling().accessDeniedPage("/accessDeniedPage");
-		
-	} 
-	
-	
-	
-		
-	
+
+		http.authorizeRequests().antMatchers("/").hasRole("EMPLOYEE").antMatchers("/leaders/**").hasRole("MANAGER")
+				.antMatchers("/systems/**").hasRole("ADMIN").and().formLogin().loginPage("/loginPage")
+				.loginProcessingUrl("/authenticateTheUser").permitAll().and().logout().permitAll().and()
+				.exceptionHandling().accessDeniedPage("/accessDeniedPage");
+
+	}
+
 }
