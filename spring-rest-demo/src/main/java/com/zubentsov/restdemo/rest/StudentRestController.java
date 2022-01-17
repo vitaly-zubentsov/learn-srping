b4fafb51051342b84ddc8824887ea636e0ca04f3
vitaly <vitaly.zubentsov@ya.ru>
2022-01-17 21:43:18 +0300

    Spring REST. Refactor generating list of students with PostConstruct annotation

--- spring-rest-demo/src/main/java/com/zubentsov/restdemo/rest/StudentRestController.java
+++ spring-rest-demo/src/main/java/com/zubentsov/restdemo/rest/StudentRestController.java
@@ -3,6 +3,8 @@ package com.zubentsov.restdemo.rest;
 import java.util.ArrayList;
 import java.util.List;
 
+import javax.annotation.PostConstruct;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -13,15 +15,24 @@ import com.zubentsov.restdemo.entity.Student;
 @RequestMapping("/api")
 public class StudentRestController {
 
-	@GetMapping("/students")
-	public List<Student> getListOfStudents() {
+	List<Student> students;
+
+	// define PostConstruct to load student ... only once
+
+	@PostConstruct 
+	public void LoadData() {
 		
-		List<Student> students = new ArrayList<>();
+List<Student> students = new ArrayList<>();
 		
 		students.add(new Student("Ivan", "Ivanov"));
 		students.add(new Student("Gleb", "Patrushev"));
 		students.add(new Student("Maria", "Ivanovna"));
 		
+	}
+
+	@GetMapping("/students")
+	public List<Student> getListOfStudents() {
+
 		return students;
 	}
 }
