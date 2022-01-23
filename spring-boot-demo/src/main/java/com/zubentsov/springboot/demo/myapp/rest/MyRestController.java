e7c5436edd6c89a918aa9063fc77e032b47fcec2
vitaly <vitaly.zubentsov@ya.ru>
2022-01-23 20:22:29 +0300

    Spring Boot. Add custom application properties

--- spring-boot-demo/src/main/java/com/zubentsov/springboot/demo/myapp/rest/MyRestController.java
+++ spring-boot-demo/src/main/java/com/zubentsov/springboot/demo/myapp/rest/MyRestController.java
@@ -2,11 +2,25 @@ package com.zubentsov.springboot.demo.myapp.rest;
 
 import java.time.LocalDateTime;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 public class MyRestController {
+	
+	//inject properties 
+	
+	@Value("${coach.name}")
+	private String coachName;
+	
+	@Value("${team.name}")
+	private String teamName;
+
+	@GetMapping("/teaminfo")
+	public String getTeamInfo() {
+		return "Coach: " + coachName + "; Team: " + teamName;
+	}
 
 	@GetMapping("/")
 	public String helloWorld() {
