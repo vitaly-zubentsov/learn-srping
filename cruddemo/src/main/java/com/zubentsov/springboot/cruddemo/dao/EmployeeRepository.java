16eeb2f4caafe8a6b4cc6c9da9163f2bb0f366a4
vitaly <vitaly.zubentsov@ya.ru>
2022-01-29 18:00:14 +0300

    Spring Boot. Add some properties to spring data rest

--- cruddemo/src/main/java/com/zubentsov/springboot/cruddemo/dao/EmployeeRepository.java
+++ cruddemo/src/main/java/com/zubentsov/springboot/cruddemo/dao/EmployeeRepository.java
@@ -1,9 +1,11 @@
 package com.zubentsov.springboot.cruddemo.dao;
 
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.zubentsov.springboot.cruddemo.entity.Employee;
 
+@RepositoryRestResource(path = "members")
 public interface EmployeeRepository extends JpaRepository<Employee, Integer> {
 
 }
