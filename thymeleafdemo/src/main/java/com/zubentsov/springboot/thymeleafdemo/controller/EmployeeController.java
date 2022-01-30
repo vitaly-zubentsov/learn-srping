fc403a84d4022c6bd4f3a446a3cc18a0b64aaaef
vitaly <vitaly.zubentsov@ya.ru>
2022-01-30 19:55:54 +0300

    Spring Boot and Thymeleaf. Add function to add new employee from web view

--- thymeleafdemo/src/main/java/com/zubentsov/springboot/thymeleafdemo/controller/EmployeeController.java
+++ thymeleafdemo/src/main/java/com/zubentsov/springboot/thymeleafdemo/controller/EmployeeController.java
@@ -4,8 +4,11 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import com.zubentsov.springboot.thymeleafdemo.entity.Employee;
 import com.zubentsov.springboot.thymeleafdemo.service.EmployeeService;
 
 @Controller
@@ -26,6 +29,22 @@ public class EmployeeController {
 		
 		model.addAttribute("employees", employeeService.findAll());
 		
-		return "list-employees";
+		return "/employees/employees-list";
+	}
+	
+	@GetMapping("/showFormForAdd")
+	public String showFormForAdd(Model model) {
+		Employee employee = new Employee();
+		
+		model.addAttribute("employee", employee);
+		
+		return "/employees/employee-form";
+	}
+	
+	@PostMapping("/save")
+	public String saveEmployee(@ModelAttribute("employee") Employee employee) {
+		employeeService.saveOrUpdate(employee);
+		//use redirect to prevent duplicate submissions 
+		return "redirect:/employees/list";
 	}
 }
--- thymeleafdemo/src/main/java/com/zubentsov/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
+++ thymeleafdemo/src/main/java/com/zubentsov/springboot/thymeleafdemo/service/EmployeeServiceImpl.java
@@ -4,6 +4,7 @@ import java.util.List;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
 
 import com.zubentsov.springboot.thymeleafdemo.dao.EmployeeDao;
 import com.zubentsov.springboot.thymeleafdemo.entity.Employee;
@@ -19,23 +20,27 @@ public class EmployeeServiceImpl implements EmployeeService {
 	}
 	
 	@Override
+	@Transactional
 	public List<Employee> findAll() {
 		
 		return employeeDao.findAll();
 	}
 
 	@Override
+	@Transactional
 	public Employee findById(int employeeId) {
 		
 		return employeeDao.findById(employeeId);
 	}
 
 	@Override
+	@Transactional
 	public void saveOrUpdate(Employee employee) {
 		employeeDao.saveOrUpdate(employee);
 	}
 
 	@Override
+	@Transactional
 	public void delete(int employeeId) {
 		employeeDao.delete(employeeId);
 	}
